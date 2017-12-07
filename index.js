629d6483e4dd031c13039c7e3239621a92a8fed8
Cedric Franz <cedric.franz@gmail.com>
2017-12-07 17:07:14 +0200

    Added commands to manage users

--- index.js
+++ index.js
@@ -22,8 +22,78 @@ var rtm = new RtmClient(token, {});
 // Web client
 var web = new WebClient(token);
 
+// variable to store ixo users
+var defaultUsers = {
+    "U6MBWN09G":{profileName: "ixo.lohan", realName: "Lohan Spies"},
+    "U6NBVNL1M":{profileName: "ixo.cedric", realName: "Cedric Franz"},
+    "U7P0T2SQ5":{profileName: "ixo.nicolaas", realName: "Nicolaas Vercuiel"}
+};
+
+var ixoUsers = JSON.parse(JSON.stringify(defaultUsers));
+
 var checkName = function(name){
     return namePattern.test(name);
+}
+
+var isIxoUser = function(userId){
+    return ixoUsers[userId]
+}
+
+var refreshIxoUsers = function(){
+    ixoUsers = {};
+    web.users.list(function(err, res){
+        if(!err){
+            res.members.forEach(function(user){
+                if(checkName(user.profile.display_name)){
+                    ixoUsers[user.id] = {
+                        profileName: user.profile.display_name, 
+                        realName: user.real_name};
+                }
+            })
+        }
+    });
+}
+
+var processPhishbert = function(message){
+    if(message.text =="phishbert list"){
+        console.log(ixoUsers);
+        web.im.open(message.user, function(err, res) {
+            var userList = "";
+            Object.keys(ixoUsers).forEach(function(k){
+                userList = userList + k + ": @" + ixoUsers[k].profileName + " (" + ixoUsers[k].realName + ")\n";
+            });
+            web.chat.postMessage(res.channel.id, userList, {
+                as_user: true,
+                parse: 'full'
+            }, function(err, res) {
+                if (err) {
+                    console.log('Error:', err);
+                } else {
+                    console.log('Posted list');
+                }
+            });
+        });
+    
+    }else if(message.text =="phishbert refresh"){
+        refreshIxoUsers();
+    }else if(message.text == "phishbert help"){
+        web.im.open(message.user, function(err, res) {
+            var msg = "help: Shows help\nlist: Lists all ixo users\nrefresh: Refreshes the list of all ixo users\nreset: Resets ixo user list\n";
+            web.chat.postMessage(res.channel.id, msg, {
+                as_user: true,
+                parse: 'full'
+            }, function(err, res) {
+                if (err) {
+                    console.log('Error:', err);
+                } else {
+                    console.log('Help sent');
+                }
+            });
+        });
+        
+    }else if(message.text =="phishbert reset"){
+        ixoUsers = JSON.parse(JSON.stringify(defaultUsers));
+    }
 }
 
 var postSecurityMessage = function(message){
@@ -57,36 +127,42 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function handleRTMAuthenticated() {
 
 // Intercept and parse every message
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+    // Check if it is a phishbert control message
+    if (message.type === 'message' && message.text.startsWith("phishbert ") && isIxoUser(message.user)){
+        processPhishbert(message);
+        console.log("Processing phishbert message: " + message.text);
+    }else{
 
-    // Check channel messages against phishing pattern
-    if (message.type === 'message' && message.channel.charAt(0) == 'C' && pattern.test(message.text)) {
-        postSecurityMessage(":warning: It looks like user ID:" + message.user + " tried to post an Ethereum address or Etherscan link that could be used for phishing/stealing funds!");
+        // Check channel messages against phishing pattern
+        if (message.type === 'message' && message.channel.charAt(0) == 'C' && pattern.test(message.text)) {
+            postSecurityMessage(":warning: It looks like user ID:" + message.user + " tried to post an Ethereum address or Etherscan link that could be used for phishing/stealing funds!");
 
-        // Delete message
-        web.chat.delete(message.ts, message.channel, {
-            as_user: true
-        }, function(err, res) {
-            if (err) {
-                console.log('Error:', err);
-            } else {
-                console.log('Message deleted: ', message);
-            }
-        });
-
-        // Warn the user (if flag is true in .env, then message from WARNING value in .env file is used)
-        if (process.env.WARN_USER == 'true') {
-            web.im.open(message.user, function(err, res) {
-                web.chat.postMessage(res.channel.id, process.env.WARNING, {
-                    as_user: true,
-                    parse: 'full'
-                }, function(err, res) {
-                    if (err) {
-                        console.log('Error:', err);
-                    } else {
-                        console.log('Used warned!');
-                    }
-                });
+            // Delete message
+            web.chat.delete(message.ts, message.channel, {
+                as_user: true
+            }, function(err, res) {
+                if (err) {
+                    console.log('Error:', err);
+                } else {
+                    console.log('Message deleted: ', message);
+                }
             });
+
+            // Warn the user (if flag is true in .env, then message from WARNING value in .env file is used)
+            if (process.env.WARN_USER == 'true') {
+                web.im.open(message.user, function(err, res) {
+                    web.chat.postMessage(res.channel.id, process.env.WARNING, {
+                        as_user: true,
+                        parse: 'full'
+                    }, function(err, res) {
+                        if (err) {
+                            console.log('Error:', err);
+                        } else {
+                            console.log('Used warned!');
+                        }
+                    });
+                });
+            }
         }
     }
 });
@@ -97,7 +173,7 @@ rtm.on(RTM_EVENTS.USER_CHANGE, function handleRtmMessage(message) {
     console.log();
     if (message.type === 'user_change'){
         console.log();
-        if(checkName(message.user.real_name) || checkName(message.user.profile.display_name) || checkName(message.user.real_name) ){
+        if(!isIxoUser(message.user.id) && (checkName(message.user.real_name) || checkName(message.user.profile.display_name)) ){
             var errMessage = ":warning: Illegal name change for @" + message.user.real_name + " (Display Name: '" + message.user.profile.display_name + "'";
             console.log(errMessage);
             postSecurityMessage(errMessage);
